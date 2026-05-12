e55bcfb53a1784ed68995ff59450024ac08ca075
St4mpe <hampusekmansundin@gmail.com>
2026-05-12 10:43:56 +0200

    1

--- schemaoverview.php
+++ schemaoverview.php
@@ -29,8 +29,22 @@ if (isset($_POST['logout']))
         </section>
         <section class="load">
             <h2 >Ladda Schema</h2>
-            <section>
-            
+            <section class="skapadescheman">
+                <?php 
+                $sqlworkout = "SELECT * FROM workouts WHERE linkeduser = {$_SESSION['loggedInUserId']}";
+                $resultworkout = mysqli_query($conn, $sqlworkout);
+
+                while($rowSchema = mysqli_fetch_assoc($resultworkout)): ?>
+                    <section class="indischeman">
+                        <?php echo $rowSchema['schedule_name']; ?>
+                        <section>
+                            <form class="load-form" action="schemaoverview.php" method="POST">
+                                <input class="ladda" type="submit" value="Ladda Schema" name="ladda"/>
+                                <input class="bort" type="submit" value="Ta Bort" name="tabort"/>
+                            </form>
+                        </section>
+                    </section>
+                <?php endwhile; ?>
             </section>
         </section>
     </section>
