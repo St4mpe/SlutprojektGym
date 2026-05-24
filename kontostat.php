3d140400a4526b815de97fa7cd4f4a0ee37ceffd
St4mpe <hampusekmansundin@gmail.com>
2026-05-24 17:27:07 +0200

    1

--- kontostat.php
+++ kontostat.php
@@ -1,6 +1,13 @@
 <?php 
 require_once("functions.php");
 
+if (isset($_POST['logout']))
+{
+    $_SESSION['userLoggedIn'] = false;
+    header("Location: index.php");
+    exit();
+}
+
 $crp=new Crypt();
 
 $sql = "SELECT completion FROM finishedworkouts WHERE linkeduser = {$_SESSION['loggedInUserId']}";
@@ -66,84 +73,91 @@ else
             <a class="selected" href="kontostat.php">Statistik</a>
         </section>
     </section>
-<section class="statruta">
-    <section class="vänster-kolumn">
-        <section class="progressbars">
-            <section class="avg-bar">
-                <h2>Genomsnittligt Genomförande</h2>
-                <section>
-                    <p id="avg-fill">0%</p>
-                    <section class="background-fill">
-                        <section class="bar-bg">
-                            <section class="avg-bar-fill"></section>
+<section>
+    <section class="statruta">
+        <section class="vänster-kolumn">
+            <section class="progressbars">
+                <section class="avg-bar">
+                    <h2>Genomsnittligt Genomförande</h2>
+                    <section>
+                        <p id="avg-fill">0%</p>
+                        <section class="background-fill">
+                            <section class="bar-bg">
+                                <section class="avg-bar-fill"></section>
+                            </section>
                         </section>
                     </section>
                 </section>
-            </section>
-            <section class="circleProgress">
-                <section class="centerHeader">
-                    <h2>Avlsutade pass av de skapade:</h2>
-                </section>
-                <section class="circle-wrapper">
-                    <section class="circle" style="--progress: <?php echo $procentage; ?>%"></section>
-                    <span data-progress="<?php echo $procentage; ?>%">Totalt</span>
-                </section>
-            </section>
-        </section>
-        <section class="sets-track-ruta">
-            <h2>Övningar</h2>
-            <section class="setListBox">
-                <?php 
-                    $sqlExLst = "SELECT * FROM excercisescompleted WHERE linkeduser={$_SESSION['loggedInUserId']} ORDER BY antal DESC";
-                    $resultExLst = mysqli_query($conn, $sqlExLst);?>
-                    <section class="setsLista">
-                        <p class="setsListaHead">Övning</p>
-                        <p class="setsListaHead">Loggade sets</p>
+                <section class="circleProgress">
+                    <section class="centerHeader">
+                        <h2>Avlsutade pass av de skapade:</h2>
                     </section>
-                    <?php
-                    while($rowLst = mysqli_fetch_assoc($resultExLst)):?>
+                    <section class="circle-wrapper">
+                        <section class="circle" style="--progress: <?php echo $procentage; ?>%"></section>
+                        <span data-progress="<?php echo $procentage; ?>%">Totalt</span>
+                    </section>
+                </section>
+            </section>
+            <section class="sets-track-ruta">
+                <h2>Övningar</h2>
+                <section class="setListBox">
+                    <?php 
+                        $sqlExLst = "SELECT * FROM excercisescompleted WHERE linkeduser={$_SESSION['loggedInUserId']} ORDER BY antal DESC";
+                        $resultExLst = mysqli_query($conn, $sqlExLst);?>
                         <section class="setsLista">
-                            <p class="setsListaEx"><?php echo $rowLst['namn'] ?></p>
-                            <p class="setsListaEx"><?php echo $rowLst['antal'] ?></p>  
+                            <p class="setsListaHead">Övning</p>
+                            <p class="setsListaHead">Loggade sets</p>
                         </section>
-                    <?php
-                    endwhile;
-                    if(mysqli_num_rows($resultExLst) == 0)
-                    {?>
-                        <section class="setsListaTom">
-                            <p>Du har inte loggat några övningar än</p>
-                        </section>
-                    <?php
-                    }
-                ?>
+                        <?php
+                        while($rowLst = mysqli_fetch_assoc($resultExLst)):?>
+                            <section class="setsLista">
+                                <p class="setsListaEx"><?php echo $rowLst['namn'] ?></p>
+                                <p class="setsListaEx"><?php echo $rowLst['antal'] ?></p>  
+                            </section>
+                        <?php
+                        endwhile;
+                        if(mysqli_num_rows($resultExLst) == 0)
+                        {?>
+                            <section class="setsListaTom">
+                                <p>Du har inte loggat några övningar än</p>
+                            </section>
+                        <?php
+                        }
+                    ?>
+                </section>
             </section>
         </section>
-    </section>
-    <section class="avklaradepass">
-        <h2>Genomförda pass</h2>
-        <section class="avklarepasslista">
-            <?php 
-                $sqlworkouts = "SELECT * FROM finishedworkouts WHERE linkeduser = {$_SESSION['loggedInUserId']}"; 
-                $resultworkouts = mysqli_query($conn, $sqlworkouts);?>
-                <section class="listoutput">
-                    <p><u>Schema:</u></p>
-                    <p><u>Procent:</u></p>
-                    <P><u>Datum:</u></P>
-                </section>
-                <?php
-                while($row = mysqli_fetch_assoc($resultworkouts)):
-                    $date = new DateTime($row['timecompleted']);?>
-                <section class="listoutput">
-                    <p><?php echo $row['workout'] ?></p>
-                    <p><?php echo $row['completion'] ?>%</p>
-                    <p><?php echo $date->format('Y-m-d');?></p>
-                </section>
-                <?php endwhile; ?>
-                <section class="historikknapp">
-                    <a href="historik.php">Se Historik</a>
-                </section>
+        <section class="avklaradepass">
+            <h2>Genomförda pass</h2>
+            <section class="avklarepasslista">
+                <?php 
+                    $sqlworkouts = "SELECT * FROM finishedworkouts WHERE linkeduser = {$_SESSION['loggedInUserId']}"; 
+                    $resultworkouts = mysqli_query($conn, $sqlworkouts);?>
+                    <section class="listoutput">
+                        <p><u>Schema:</u></p>
+                        <p><u>Procent:</u></p>
+                        <P><u>Datum:</u></P>
+                    </section>
+                    <?php
+                    while($row = mysqli_fetch_assoc($resultworkouts)):
+                        $date = new DateTime($row['timecompleted']);?>
+                    <section class="listoutput">
+                        <p><?php echo $row['workout'] ?></p>
+                        <p><?php echo $row['completion'] ?>%</p>
+                        <p><?php echo $date->format('Y-m-d');?></p>
+                    </section>
+                    <?php endwhile; ?>
+                    <section class="historikknapp">
+                        <a href="historik.php">Se Historik</a>
+                    </section>
+            </section>
         </section>
     </section>
 </section>
+<section class="form">
+        <form class="logout-form" action="schemaoverview.php" method="POST">
+            <input class="logoutbutton" type="submit" value="Logga ut" name="logout"/>
+        </form>
+    </section>
 </body>
 </html>
