182e207d9dfb818cdd7ddcb698c1c2e07c916c29
St4mpe <hampusekmansundin@gmail.com>
2026-05-29 11:24:44 +0200

    1

--- schemabuilder.sql
+++ schemabuilder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 24 maj 2026 kl 15:29
+-- Tid vid skapande: 29 maj 2026 kl 11:24
 -- Serverversion: 10.4.32-MariaDB
 -- PHP-version: 8.2.12
 
@@ -34,17 +34,6 @@ CREATE TABLE `excercisescompleted` (
   `linkeduser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumpning av Data i tabell `excercisescompleted`
---
-
-INSERT INTO `excercisescompleted` (`id`, `namn`, `antal`, `linkeduser`) VALUES
-(9, '\n            Squat\n            \n        ', 2, 6),
-(10, '\n            1\n            \n        ', 4, 6),
-(11, '\n            Bench\n            \n        ', 6, 6),
-(12, '\n            Incline Dumbell Press\n            \n        ', 1, 6),
-(13, '\n            Bänk\n            \n        ', 2, 8);
-
 -- --------------------------------------------------------
 
 --
@@ -59,14 +48,6 @@ CREATE TABLE `finishedworkouts` (
   `linkeduser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumpning av Data i tabell `finishedworkouts`
---
-
-INSERT INTO `finishedworkouts` (`id`, `workout`, `completion`, `timecompleted`, `linkeduser`) VALUES
-(69, 'ag', 75, '2026-05-24 12:55:39', 6),
-(70, 'Bänkaren', 100, '2026-05-24 13:07:48', 8);
-
 -- --------------------------------------------------------
 
 --
@@ -79,14 +60,6 @@ CREATE TABLE `userinfo` (
   `pass` text NOT NULL,
   `mail` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumpning av Data i tabell `userinfo`
---
-
-INSERT INTO `userinfo` (`id`, `user`, `pass`, `mail`) VALUES
-(6, 'BPPabbGFWVfTFXdjGij51LOcNBjrzIsW21RSFN9MkBYJ6cU/W5qUf6auyom55fZRp+nCp+KRRQ6fQS7mjz+FBQ==', 'a5aefd7730f1012d998dbe609a116ad6', 'XhoXgd50NzEvdUDJ6du06TM3xBZX8602Iily06msgxDrlHvjFNVQ2TKmiPfyvJ7QuqvGpre+hUbcirKi5VflTA=='),
-(8, 'PUH6Cy//HgMP80Po9u57vo5I6/H7FuR6QYr+nBfTCay5PEM02D3xLdDwBTcm255TFElu+BMJa74kWFQIg7LWaQ==', 'a5aefd7730f1012d998dbe609a116ad6', 'jH8o31M2GtAgCZnVdss7YkqdeOhB61gsd7tHReHQgcrKLbu9IBUyFLoWKprwcqXZNCiJbHY8oFx0kordV9cHCQ==');
 
 -- --------------------------------------------------------
 
@@ -102,14 +75,6 @@ CREATE TABLE `workouts` (
   `isfromemplate` tinyint(1) NOT NULL,
   `completed` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumpning av Data i tabell `workouts`
---
-
-INSERT INTO `workouts` (`id`, `schedule_name`, `data`, `linkeduser`, `isfromemplate`, `completed`) VALUES
-(92, 'ag', '{\n  \"scheduleName\": \"ag\",\n  \"exercises\": [\n    {\n      \"exercise\": 1,\n      \"name\": \"Bench\",\n      \"sets\": [\n        {\n          \"set\": 1,\n          \"reps\": \"1\",\n          \"weight\": \"2\",\n          \"rpe\": \"3\"\n        },\n        {\n          \"set\": 2,\n          \"reps\": \"2\",\n          \"weight\": \"3\",\n          \"rpe\": \"2\"\n        }\n      ]\n    },\n    {\n      \"exercise\": 2,\n      \"name\": \"Incline Dumbell Press\",\n      \"sets\": [\n        {\n          \"set\": 1,\n          \"reps\": \"2\",\n          \"weight\": \"2\",\n          \"rpe\": \"2\"\n        },\n        {\n          \"set\": 2,\n          \"reps\": \"3\",\n          \"weight\": \"3\",\n          \"rpe\": \"3\"\n        }\n      ]\n    }\n  ]\n}', 6, 0, 1),
-(93, 'Bänkaren', '{\n  \"scheduleName\": \"Bänkaren\",\n  \"exercises\": [\n    {\n      \"exercise\": 1,\n      \"name\": \"Bänk\",\n      \"sets\": [\n        {\n          \"set\": 1,\n          \"reps\": \"1\",\n          \"weight\": \"1\",\n          \"rpe\": \"1\"\n        },\n        {\n          \"set\": 2,\n          \"reps\": \"1\",\n          \"weight\": \"1\",\n          \"rpe\": \"1\"\n        }\n      ]\n    }\n  ]\n}', 8, 0, 1);
 
 --
 -- Index för dumpade tabeller
@@ -147,25 +112,25 @@ ALTER TABLE `workouts`
 -- AUTO_INCREMENT för tabell `excercisescompleted`
 --
 ALTER TABLE `excercisescompleted`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT för tabell `finishedworkouts`
 --
 ALTER TABLE `finishedworkouts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT för tabell `userinfo`
 --
 ALTER TABLE `userinfo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT för tabell `workouts`
 --
 ALTER TABLE `workouts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
